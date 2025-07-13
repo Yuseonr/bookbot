a537727e0142ffbfe19b3b3db987029cfb79ad5a
Yuseonr <rafianandra@gmail.com>
2025-07-13 15:20:45 +0700

    Add get char count function to stats.py

--- main.py
+++ main.py
@@ -2,14 +2,17 @@
 
 from stats import get_words_count
 
+from stats import get_char_count
+
 def main():
 
     book = get_book_text("books/frankenstein.txt")
     words_count = get_words_count(book)
+    alphabet_count = get_char_count(book)
 
     # print(book)
-
     print(f"{words_count} words found in the document")
+    print(alphabet_count)
     
 
 def get_book_text(file_path):
--- stats.py
+++ stats.py
@@ -2,3 +2,11 @@
 
 def get_words_count(file_string):
     return len(file_string.split())
+
+def get_char_count(file_string):
+    char_dict = {} 
+    for char in file_string.lower():
+            char_dict[char] = char_dict.get(char, 0) + 1
+    return char_dict
+
+   

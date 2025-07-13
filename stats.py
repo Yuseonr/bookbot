fc9c141e109b92d22a04f5d01c83fb139555fc43
Yuseonr <rafianandra@gmail.com>
2025-07-13 16:30:13 +0700

    Add sorted char count function to stats.py

--- stats.py
+++ stats.py
@@ -9,4 +9,13 @@ def get_char_count(file_string):
             char_dict[char] = char_dict.get(char, 0) + 1
     return char_dict
 
-   
+def get_sort_char_count(char_count_dict) :
+    list_of_char_dict = []
+    for i in char_count_dict :
+        char_d = {}
+        char_d['char'] = i
+        char_d['num'] = char_count_dict[i]
+        list_of_char_dict.append(char_d)
+    list_of_char_dict.sort(reverse=True, key=lambda x: x['num'])
+    return list_of_char_dict
+

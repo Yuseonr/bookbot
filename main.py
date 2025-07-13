7702d966ddb488657e48dc1d0aa06ade73919c43
Yuseonr <rafianandra@gmail.com>
2025-07-13 14:25:53 +0700

    Add Read Function to main.py

--- main.py
+++ main.py
@@ -1,1 +1,12 @@
-print("greetings boots")
+# 13 Jul 2025
+
+def main():
+    print(get_book_text("books/frankenstein.txt"))
+
+
+def get_book_text(file_path):
+    with open (file_path) as f:
+        return f.read()
+
+
+main()
